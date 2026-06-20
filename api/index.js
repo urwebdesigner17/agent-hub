93b3c8657841b59f1658e1d151e2c3e00d97fac6
Robert Fuentes <robert.f@ideahubsolutionsinc.com>
2026-06-20 21:41:33 +0800

    set up Mongo DB database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB).then(()=>{
+    console.log('connected to MongoDB')
+}).catch((err)=>{
+    console.log(err)
+})
 
 const app = express();
 
