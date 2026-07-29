25f06605d1a4f908b7ab214fec543f1aa4e9e242
Robert Fuentes <robert.f@ideahubsolutionsinc.com>
2026-07-29 17:04:20 +0800

    adding redux persist middleware

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import {persistReducer, persistStore} from 'redux-persist'
+import storageImport from 'redux-persist/lib/storage'
+
+const storage = storageImport.default ? storageImport.default : storageImport;
+
+//this handle to remember or store user data in teh browser
+const rootReducer = combineReducers({user: userReducer}) //will pass this userReducer to persitConfig
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer) //combining the two storage to work data local and in browser
 
 //ensure to install npm install @reduxjs/toolkit react-redux in the client side
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
         serializableCheck: false,
     })
 })
+
+export const persistor = persistStore(store);
