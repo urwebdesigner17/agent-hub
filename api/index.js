f55869097494f5c14bfe4cc720ee52615d41fabe
Robert Fuentes <robert.f@ideahubsolutionsinc.com>
2026-08-10 17:25:57 +0800

    User update API created

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import userRouter from '../api/routes/user.route.js'
 import authRouter from '../api/routes/auth.route.js'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
@@ -14,19 +15,20 @@ mongoose.connect(process.env.MONGODB).then(()=>{
 
 const app = express();
 
+app.use(express.json())
+app.use(cookieParser())
+
 app.listen(3000, ()=>{
     console.log('Server running on Port 3000');
     }
 )
-
-app.use(express.json()) //this is use when requesting json data
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
 //ERROR MIDDLEWARE
 app.use((err, req, res, next) => {
-    const statusCode = err.statuscode || 500; //if no error defined just send 500 internal server error
+    const statusCode = err.statuscode || 500;
     const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
         success: false,
